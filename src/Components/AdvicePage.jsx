78ec579fba6fdea623c625c069f4b6f695ccf145
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-12-14 09:59:22 -0500

    set up button for advice page, and got text to show on screen for quotes

--- src/Components/AdvicePage.jsx
+++ src/Components/AdvicePage.jsx
@@ -1,5 +1,45 @@
+import { useState, useEffect } from "react";
+
 const AdvicePage = () => {
-  return <div>AdvicePage</div>;
+  const [maxxQuotes, setMaxxQuotes] = useState([
+    "hello",
+    "goodbye",
+    "I am the Maxx",
+  ]);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentAdvice = maxxQuotes[currentIndex];
+
+  const handleClick = () => {
+    const pickRandomIndex = Math.floor(Math.random() * maxxQuotes.length);
+
+    setCurrentIndex(pickRandomIndex);
+
+    console.log(maxxQuotes);
+  };
+
+  // will need if we store data for quotes/audio in another file, which is likely
+  // useEffect(() => {
+  // }, []);
+
+  return (
+    <>
+      <div>
+        <h1>AdvicePage</h1>
+        <div>
+          <p>Well you made it this far... come on, pick my brain.</p>
+          <button onClick={handleClick} className="ask-maxx-bttn">
+            Ask The Maxx
+          </button>
+          {maxxQuotes && (
+            <>
+              (<p>{currentAdvice}</p>)
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AdvicePage;
