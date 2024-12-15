6841b9221bbd797bf6a2fd634fd2446e4687a456
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-12-15 09:54:33 -0500

    adding more quotes + rethinking css for advice pg

--- src/Components/AdvicePage.jsx
+++ src/Components/AdvicePage.jsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from "react";
 
 const AdvicePage = () => {
-  const [maxxQuotes, setMaxxQuotes] = useState([
-    "hello",
-    "goodbye",
-    "I am the Maxx",
-  ]);
+  const maxxQuotes = [
+    "I am the Maxx.",
+    "I don't have a TV now, but that's okay. The shows in my mind are almost always better.",
+    "Sometimes it's luck that saves them. Sometimes it's fate.",
+    "Great. Where the heck am I now?",
+    "These hands - each as strong as a man, hungry as death - they will drag a man down to nothingness. To oblivion! It's starting to fade. It's starting to come back to me now. Back to Australia. Not the Australia they teach you about in the cities - the Outback. No, the *real* Australia: the smoking caves, the gods who eat mountains, the flying dragos, the rivers of gold, the Leopard Queen taunting me, almost an enemy, beautiful and feral as the great beasts she runs with. Still, she inspires me. For her I can be...",
+    "It has no predators; it is just... stupid.",
+    "Answer your phone.",
+    "Then it wasn't all a dream. Unless I'm dreaming now.",
+    "She helped people by controlling them, hoping to smother her own pain. And, somewhere in the wild land, in the land of dreams, in Pangaea, her inner-self was imprisoned by her own self-loathing.",
+    "OUCHIE.",
+    "She helped by controlling them...hoping to smother her own pain...something like that.",
+    "",
+  ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const currentAdvice = maxxQuotes[currentIndex];
@@ -27,9 +36,9 @@ const AdvicePage = () => {
 
   return (
     <>
-      <div>
-        <h1>AdvicePage</h1>
-        <div>
+      <div style={{ backgroundColor: "black" }}>
+        <h1>Advice Page</h1>
+        <div className="advice-page-wrapper">
           <p>Well you made it this far... come on, pick my brain.</p>
           <button onClick={handleClick} className="ask-maxx-bttn">
             Ask The Maxx
